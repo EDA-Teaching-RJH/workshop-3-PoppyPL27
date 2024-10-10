ea26e521f81f0f141aa038572a16c09e179cfe2a
PoppyPL27 <pp520@kent.ac.uk>
2024-10-10 13:53:22 +0000

    starting on the text based adventure game

--- partEight.py
+++ partEight.py
@@ -1,1 +1,11 @@
+##the magnus archives text based adventure game
+def main():
+    EnterAtYourOwnPeril = input("welcome to The Magnus archives: a Text based adventure game. Are you ready to begin?")
+    if EnterAtYourOwnPeril == "yes":
+        print("You may regret your Eagerness")
+    else:
+        print("oh you fool. you never had a choice. i just asked to be polite")
+    print("new statement loading ...")
 
+
+main()
